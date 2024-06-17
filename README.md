4337d1919deb04d3f3eb1f23267cb0a11217ea5f
Blitzkreig <crhlstl4nk09@gmail.com>
2024-06-16 19:04:46 -0600

    Retoques necesarios

--- README.md
+++
@@ -1,4 +0,0 @@
-# Plantilla de un Blog
-
-Esta plantilla es de un blog inicial que tenía como proyecto y puede ser usado por cualquier persona.
-No olvide instalar sus respectivas dependencias.
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
             <footer className="flex flex-col justify-center items-center py-7">
                 <div className="py-4 flex flex-row gap-3">
                 </div>
-                <small className="">Creado por <a href="https://github.com/Blitzkreig/" target="_BLANK"><strong>Blitzkreig</strong></a></small>
+                <small className="">Creado por <a href="https://github.com/Blitzkreigx/" target="_BLANK"><strong>Blitzkreig</strong></a></small>
             </footer>
         </>
     );
